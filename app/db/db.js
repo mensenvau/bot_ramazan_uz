4e03d1ca7d0fd097fb889e7f2368ab181b12f235
mensenvau <otkir8311@gmail.com>
2021-04-12 18:19:46 +0500

    3-kun

--- app/db/db.js
+++ app/db/db.js
@@ -4,7 +4,7 @@ const mysql = require('mysql2');
 const pool = mysql.createPool({
     host: 'localhost',
     user: 'root',
-    password: "@Aa12345",
+    password: "12345",
     database: 'botc',
     waitForConnections: true,
     connectionLimit: 10,
